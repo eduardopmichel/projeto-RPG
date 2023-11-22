391c3f74f561335faa3f7fd067a463a9c62b15e5
Eduardo Peres Michel <47788458+eduardopmichel@users.noreply.github.com>
2023-11-22 11:23:02 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,30 +1,68 @@
-async function carregarDados() {
-    // Lê dados do servidor
-    const resposta = await fetch('dados.json');
-    return resposta.json();
+
+let coll = document.getElementsByClassName("collapsible");
+let i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
 }
 
-function rolarDados20(bonus, vantagem) {
-    let resultado;
+function dadosVinte(status, vantagem) {
+    let bonus = calcularBonus(status)
+    let resultado = bonus;
     let isCritical = false;
     const dado1 = Math.floor(Math.random() * 20) + 1;
-    ;
-    if (vantagem === "adv") {
-        const dado2 = Math.floor(Math.random() * 20) + 1
-        resultado = Math.max(dado1, dado2);
-    } else if (vantagem === "dis") {
-        const dado2 = Math.floor(Math.random() * 20) + 1
-        resultado = Math.min(dado1, dado2);
+
+    if(vantagem === "adv"){
+        const dado2 = Math.floor(Math.random() * 20) + 1;
+        resultado += Math.max(dado1, dado2);
+    }else if(vantagem === "dis"){
+        const dado2 = Math.floor(Math.random() * 20) + 1;
+        resultado += Math.min(dado1, dado2);
+    }else {
+        resultado += dado1;
     }
-    if(resultado === 20){
-        isCritical = true
+    
+    if((resultado - bonus) === 20){
+        isCritical = true;
     }
-    return {
+
+    return dVinte = {
         resultado: resultado,
-        bonus: bonus,
-        total: resultado+bonus,
         critico: isCritical
     };
-  }
+}
 
-console.log(rolarDados20(2,"adv"))
+function dadosNormal(rolagens, lados, critico, status){
+    const dados = [];
+    let resultado = calcularBonus(status);
+
+    if(critico){
+        rolagens*=2;
+    }
+    for (let i = 0; i < rolagens; i++) {
+        const dado = Math.floor(Math.random() * lados) + 1;
+        dados.push(dado);
+        resultado += dado;
+    }
+    return {
+        dados: dados,
+        resultado: resultado
+    }
+}
+
+function calcularBonus(status){
+    let modificadores = [-10, -5, -4, -4, -3, -3, -2, -2, -1, -1, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5];
+    return modificadores[status];
+}
+
+console.log(dadosVinte(20));
+
+console.log(dadosNormal(2,6,dVinte.critico,12));
